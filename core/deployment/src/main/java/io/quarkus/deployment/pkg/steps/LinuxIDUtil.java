7a6e5887d0689695d23192547126b1e3d332bb28
Foivos Zakkak <fzakkak@redhat.com>
2025-08-26 10:57:21 +0300

    TESTME

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/LinuxIDUtil.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/LinuxIDUtil.java
@@ -1,6 +1,7 @@
 package io.quarkus.deployment.pkg.steps;
 
 import io.smallrye.common.process.ProcessBuilder;
+import org.jboss.logging.Logger;
 
 final class LinuxIDUtil {
 
@@ -11,6 +12,7 @@ final class LinuxIDUtil {
         try {
             return ProcessBuilder.execToString("id", option).trim();
         } catch (Exception e) {
+            Logger.getLogger(LinuxIDUtil.class.getPackageName()).warn("Failed to read ID", e);
             //swallow and return null id
             return null;
         }
